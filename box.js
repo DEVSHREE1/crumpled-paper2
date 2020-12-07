2a15bd950f4822943ebe1e3988cf75203153bd4b
DEVSHREE1 <73652948+DEVSHREE1@users.noreply.github.com>
2020-12-07 19:54:59 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,76 @@
+class Box
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=200;
+		this.dustbinHeight=210;
+		this.wallThickness=20;
+		//this.angle=0;	
+		
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		//Matter.Body.setAngle(this.leftWallBody, this.angle);
+		this.image = loadImage("sprites/dustbingreen.png");
+
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		//Matter.Body.setAngle(this.rightWallBody, -1*this.angle);
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			push()
+			translate(posLeft.x, posLeft.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill("pink")
+			stroke("pink")
+
+			rotate(this.angle)
+            rect(0,-5,this.wallThickness, this.dustbinHeight);
+            
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			
+			angleMode(RADIANS)
+            fill("pink")
+            stroke("pink")
+			rotate(-1*this.angle)
+            rect(0,-5,this.wallThickness, this.dustbinHeight);
+           // rect(0,0,this.dustbinWidth, this.wallThickness);
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+		
+			angleMode(RADIANS)
+            fill("pink")
+            stroke("pink")
+			rect(0,0,this.dustbinWidth, this.wallThickness);
+
+			imageMode(CENTER);
+			image(this.image, 0,-100,200,230 )
+			pop()
+
+
+
+			
+			
+	}
+
+}
---
+++ crumbledpaper.js
@@ -0,0 +1,38 @@
+class Crumbledpaper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+		this.image = loadImage("sprites/paper.png");
+
+	}
+	display()
+	{
+
+			var paperpos=this.body.position;		
+            this.scale = 0.1;
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(0);
+			fill("pink");
+			ellipse(0,0,this.r, this.r);
+			imageMode(CENTER);
+            image(this.image, 0, 0, 60,60);
+			pop()
+
+	}
+}
